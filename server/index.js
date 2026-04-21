6a29e2259b54b462f039a6829a7a31badf870435
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-21 09:54:31 +0600

    Mongodb connection Successfull

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 // import cors from 'cors';
 import http from 'http';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
 
 // Load environment variables from .env file
 dotenv.config();
@@ -13,8 +15,12 @@ const app = express();
 // Create an HTTP server using the Express app
 const server = http.createServer(app);
 
+
+
 // Start the server
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
+    // Connect to the database
+    connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
