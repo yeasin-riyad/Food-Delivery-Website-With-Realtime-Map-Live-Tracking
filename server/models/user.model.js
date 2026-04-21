e26060872350e9a887f200c695333b2074ed69d9
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-04-21 10:02:55 +0600

    User model done

---
+++ server/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "owner", "deliveryBoy"],
+      required: true,
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
